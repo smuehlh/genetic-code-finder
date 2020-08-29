b1add7795d06d816d934b5ee8ffb73d4b5e583f3
Stefanie Mühlhausen <s.muehlhausen@bath.ac.uk>
2020-08-29 15:47:56 +0200

    Iterate input sequences

--- 01_create_maxquant_db.rb
+++ 01_create_maxquant_db.rb
@@ -5,9 +5,14 @@ require 'optparse'
 =begin
     Create a FASTA file to be used as MaxQuant database.
 
+    Skip all sequences that cannot be splitted into codons (whose length is
+    not a multiple of 3)
+
+
     Args:
         input (str): path input FASTA (cDNA sequences)
         output (str): path to output FASTA (protein sequences)
+        map (str): path to output CSV (dictionary of simplified gene names)
 =end
 
 # require .rb files in library (including all subfolders)
@@ -23,6 +28,7 @@ class OptParser
         # mandatory parameters
         options[:input] = nil
         options[:output] = nil
+        options[:map] =nil
 
         opt_parser = OptionParser.new do |opts|
             opts.banner = "Generate FASTA file to be used as MaxQuant database."
@@ -32,7 +38,7 @@ class OptParser
             opts.separator "This program comes with ABSOLUTELY NO WARRANTY"
 
             opts.separator ""
-            opts.separator "Usage: ruby #{File.basename($PROGRAM_NAME)} -i input -o output"
+            opts.separator "Usage: ruby #{File.basename($PROGRAM_NAME)} -i input -o output -m dict"
 
             opts.on("-i", "--input FILE",
                 "Path to input file, in FASTA format.") do |path|
@@ -42,6 +48,11 @@ class OptParser
             opts.on("-o", "--output FILE",
                 "Path to output file, in FASTA format.") do |path|
                 options[:output] = path
+            end
+            opts.on("-m", "--map FILE",
+                "Path to auxially output file mapping original ",
+                "to simplified FASTA headers, in CSV format.") do |path|
+                options[:map] = path
             end
             opts.separator ""
             opts.on_tail("-h", "--help", "Show this message") do
@@ -61,8 +72,29 @@ class OptParser
         # ensure mandatory arguments are present
         abort "Missing mandatory argument: --input" unless options[:input]
         abort "Missing mandatory argument: --output" unless options[:output]
+        abort "Missing mandatory argument: --map" unless options[:map]
         return options
     end
 end
 
+def simplify_header(num)
+    "g#{num}"
+end
+
 options = OptParser.parse(ARGV)
+
+headers, seqs = Sequence.read_fasta(options[:input])
+fh_map = File.open(options[:map], "w")
+seqs.each_with_index do |seq, ind|
+    orig_header = headers[ind]
+    patched_header = simplify_header(ind+1)
+    fh_map.puts "#{orig_header};#{patched_header}"
+
+    codons = Sequence.split_cdna_into_codons(seq)
+    if codons.last.size != 3
+        puts "Skip #{orig_header}, cannot split cDNA into codons"
+        next
+    end
+
+end
+fh_map.close
---
+++ lib/sequence.rb
@@ -0,0 +1,81 @@
+module Sequence
+    extend self
+
+    def genetic_code
+        {
+            "TTT" => "F", "TTC" => "F",
+            "TTA" => "L", "TTG" => "L", "CTT" => "L", "CTC" => "L",
+                "CTA" => "L", "CTG" => "L",
+            "ATT" => "I", "ATC" => "I", "ATA" => "I",
+            "ATG" => "M",
+            "GTT" => "V", "GTC" => "V", "GTA" => "V", "GTG" => "V",
+            "TCT" => "S", "TCC" => "S", "TCA" => "S", "TCG" => "S",
+                "AGT" => "S", "AGC" => "S",
+            "CCT" => "P", "CCC" => "P", "CCA" => "P", "CCG" => "P",
+            "ACT" => "T", "ACC" => "T", "ACA" => "T", "ACG" => "T",
+            "TAT" => "Y", "TAC" => "Y",
+            "GCT" => "A", "GCC" => "A", "GCA" => "A", "GCG" => "A",
+            "CAT" => "H", "CAC" => "H",
+            "CAA" => "Q", "CAG" => "Q",
+            "AAT" => "N", "AAC" => "N",
+            "AAA" => "K", "AAG" => "K",
+            "GAT" => "D", "GAC" => "D",
+            "GAA" => "E", "GAG" => "E",
+            "TGT" => "C", "TGC" => "C",
+            "TGG" => "W",
+            "CGT" => "R", "CGC" => "R", "CGA" => "R", "CGG" => "R",
+                "AGA" => "R", "AGG" => "R",
+            "GGT" => "G", "GGC" => "G", "GGA" => "G", "GGG" => "G",
+            "TAA" => "*", "TAG" => "*", "TGA" => "*"
+        }
+    end
+
+    def is_stopcodon(codon)
+        genetic_code.fetch(codon, "") == "*"
+    end
+
+    def translate(cdna)
+        codons = split_cdna_into_codons(cdna)
+        delete_trailing_stopcodon_if_present(codons)
+        codons.map do |codon|
+            genetic_code.fetch(codon).upcase
+        end.join("")
+    end
+
+    def split_cdna_into_codons(cdna)
+        cdna.scan(/.{1,3}/)
+    end
+
+    def delete_trailing_stopcodon_if_present(codons)
+        if is_stopcodon(codons.last)
+            codons.pop
+        end
+    end
+
+    def read_fasta(path)
+        headers_with_seqs = {}
+        current_header = ""
+        IO.foreach(path) do |line|
+            line.chomp!
+            if line.start_with?(">")
+                # fasta header
+                current_header = line[1..-1]
+                if headers_with_seqs[current_header]
+                    abort "duplicate FASTA header #{current_header}"
+                end
+                headers_with_seqs[current_header] = ""
+            else
+                # fasta sequence
+                headers_with_seqs[current_header] += line
+            end
+        end
+        [headers_with_seqs.keys, headers_with_seqs.values]
+    end
+
+    def write_fasta(fh, header, seq)
+        header = ">#{header}" unless header.start_with?(">")
+        fh.puts header
+        fh.puts seq.scan(/.{1,80}/).join("\n")
+    end
+
+end
